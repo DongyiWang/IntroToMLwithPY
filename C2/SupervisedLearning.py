be8aee888b800eb0ab2db5c2ed4a7cb2124084a8
DongyiWang <dywang@terpmail.umd.edu>
2017-06-30 20:28:05 -0400

    06/30/2017

--- C2/SupervisedLearning.py
+++ C2/SupervisedLearning.py
@@ -112,3 +112,25 @@ for n_neighbors, ax in zip([1, 3, 9], axes):
     ax.set_ylabel("Target")
 axes[0].legend(["Model predictions", "Training data/target",
     "Test data/target"], loc="best")
+
+mglearn.plots.plot_linear_regression_wave()
+
+from sklearn.linear_model import LinearRegression
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lr = LinearRegression().fit(X_train, y_train)
+
+print("lr.coef_:{}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test))) 
+
+from sklearn.linear_model import Ridge
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
